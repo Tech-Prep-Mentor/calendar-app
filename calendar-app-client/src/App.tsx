8ba0d03b146f103f97240dcc4cdc54bafa521800
SonNguyen <nguyenminhson20051129@gmail.com>
2024-03-03 02:55:50 -0500

    "user calendar"

--- calendar-app-client/src/App.tsx
+++ calendar-app-client/src/App.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import Feature from './pages/Home/Feature';
-import Hero from './pages/Home/Hero';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Feature from "./pages/Home/Feature";
+import Hero from "./pages/Home/Hero";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Calendar from "./pages/calendar/Calendar";
 function App() {
   return (
-    <div className="App">
-    <Navbar />
-    <Hero />
-    <Feature/>
-    <Footer/>
-  </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Navbar />
+                <Hero />
+                <Feature />
+                <Footer />
+              </div>
+            }
+          />
+          <Route
+            path="/calendar"
+            element={
+              <div>
+                <Calendar />
+              </div>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
